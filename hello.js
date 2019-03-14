2048c934253bfe6619a0c2a2f71dd87d4d77dc4b
Yongchan Kim <longchan@nara.co.kr>
2019-03-14 18:18:47 +0900

    initial commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+function max(arg1, arg2) {
+    var arg1isLarger = arg1 > arg2;
+    console.log(arg1isLarger);
+    if (arg1isLarger) {
+        return arg1;
+    } else {
+        return arg2;
+    }
+}
+
+function max2(arg1, arg2, arg3) {
+    return max(max(arg1, arg2), arg3);
+}
